538b8bb313279992366414a1dffd0c0887f0e726
PalmForest <realpalmforest@gmail.com>
2025-12-26 15:31:16 +1100

    Create Interfaces folder

--- BloomEngine/Interfaces/IInputField.cs
+++ BloomEngine/Interfaces/IInputField.cs
@@ -16,13 +16,3 @@ public interface IInputField
     void RefreshUI();
     void OnUIChanged();
 }
-
-public interface IInputField<T> : IInputField
-{
-    Action<T> OnValueChanged { get; set; }
-    Action OnInputChanged { get; set; }
-    Func<T, T> TransformValue { get; set; }
-    Func<T, bool> ValidateValue { get; set; }
-
-    T Value { get; set; }
-}
---
+++ BloomEngine/Interfaces/IInputFieldT.cs
@@ -0,0 +1,9 @@
+public interface IInputField<T> : IInputField
+{
+    Action<T> OnValueChanged { get; set; }
+    Action OnInputChanged { get; set; }
+    Func<T, T> TransformValue { get; set; }
+    Func<T, bool> ValidateValue { get; set; }
+
+    T Value { get; set; }
+}
