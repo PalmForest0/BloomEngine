3bbcd9088f0211a66ffdb7d27f7541bdf8ceb09b
PalmForest <realpalmforest@gmail.com>
2026-09-12 11:17:54 +1000

    Change mod colour to light green

--- src/Core/AssemblyInfo.cs
+++ src/Core/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 
 [assembly: MelonInfo(typeof(BloomEngineMod), BloomEngineMod.Name, BloomEngineMod.Version, BloomEngineMod.Author)]
 [assembly: MelonGame("PopCap Games", "PvZ Replanted")]
-[assembly: MelonColor(255, 105, 115, 195)]
+[assembly: MelonColor(255, 120, 220, 100)]
 [assembly: ComVisible(false)]
